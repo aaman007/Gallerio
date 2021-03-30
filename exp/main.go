601bd5b430455aa6d1005f621685f981d94b28d5
Amanur Rahman <aaman.sunny007@gmail.com>
2021-03-30 22:02:44 +0600

    Added rand package

--- exp/main.go
+++ exp/main.go
@@ -1,48 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"go-web-dev-2/accounts"
-)
-
-const (
-	dbHost = "localhost"
-	dbPort = 5432
-	dbUser = "robert"
-	dbPassword = "password"
-	dbName = "gallerio"
-)
-
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		dbHost, dbPort, dbUser, dbPassword, dbName,
-	)
-
-	us, err := accounts.NewService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-
-	defer us.Close()
-
-	user := accounts.User{
-		Name: "Amanur Rahman",
-		Email: "aaman007.liilab@gmail.com",
-	}
-
-	us.DestructiveReset()
-	if err := us.Create(&user); err != nil {
-		panic(err)
-	}
-
-	err = us.Delete(0)
-	if err != nil {
-		panic(err)
-	}
-
-	userById, err := us.ByID(1)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(userById)
 }
