f4bbd955066aaf04e22329d9387293fce17ea6cd
Amanur Rahman <aaman.sunny007@gmail.com>
2021-04-01 19:28:18 +0600

    Added release script format

---
+++ scripts/release.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Change directory where application is in
+cd "$GOPATH/src/your_app" || exit
+
+echo "================= Releasing Gallerio ================="
+
+
+echo "Removing local binary if exits (It shouldn't be uploaded)"
+
+
+echo "--- Deleting existing code ---"
+
+
+echo "--- Uploading Code ---"
+
+
+echo "--- Uploading Code ---"
+
+
+echo "--- Getting Dependency ---"
+
+
+echo "--- Building the code on remote server ---"
+
+
+echo "--- Moving static files ----"
+
+
+echo "--- Moving views ----"
+
+
+echo "--- Moving CaddyFile ---"
+
+
+echo "--- Restarting Server ---"
+
+
+echo "--- Restarting Caddy ---"
+
+
+echo "================= Done Releasing Gallerio ================="
